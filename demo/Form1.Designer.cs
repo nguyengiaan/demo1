85c16e052049b939c7edfa930980cc1319383e34
An Professional <An Professional@DESKTOP-PGHJH2J>
2022-07-14 07:32:34 +0700

    heldjad

--- demo/Form1.Designer.cs
+++ demo/Form1.Designer.cs
@@ -35,10 +35,11 @@
             // 
             this.button1.Location = new System.Drawing.Point(322, 150);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(110, 72);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
--- demo/Form1.cs
+++ demo/Form1.cs
@@ -21,5 +21,10 @@ namespace demo
         {
             MessageBox.Show("hello");
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
